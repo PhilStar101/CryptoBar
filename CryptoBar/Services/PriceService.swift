2caaa951bbc49cabb22de61f24498ca63964072c
Phil Star <vipfilstar@gmail.com>
2022-03-03 16:33:32 +0300

    Price service

--- CryptoBar/Services/PriceService.swift
+++ CryptoBar/Services/PriceService.swift
@@ -5,4 +5,21 @@
 //  Created by Philipp Starchenko on 03.03.2022.
 //
 
+import Combine
 import Foundation
+
+class PriceService {
+    private let session = URLSession(configuration: .default)
+    private var wsTast: URLSessionDataTask?
+
+    private let coinDictionarySubject = CurrentValueSubject<[String: Coin], Never>([:])
+    private var coinDictionary: [String: Coin] { coinDictionarySubject.value }
+
+    private let connectionSubject = CurrentValueSubject<Bool, Never>(false)
+    private var isConnected: Bool { connectionSubject.value }
+
+    deinit {
+        coinDictionarySubject.send(completion: .finished)
+        connectionSubject.send(completion: .finished)
+    }
+}
